b17be453a6da5d78f268e2e96786934ef6a8f474
Tomasz Szumski <tomasz.szumski@intel.com>
2024-04-02 19:24:22 +0200

    AVX512 implementation of deadzone quantization

---
+++ Source/Lib/Encoder/ASM_AVX512/Quant_avx512.c
@@ -0,0 +1,72 @@
+/*
+* Copyright(c) 2024 Intel Corporation
+* SPDX - License - Identifier: BSD - 2 - Clause - Patent
+*/
+
+#include "Quant_avx512.h"
+#include "Quant_sse4_1.h"
+#include "Definitions.h"
+#include "Quant.h"
+#include <immintrin.h>
+
+static void quant_deadzone_avx512(uint16_t* buf, uint32_t size, int8_t gtli) {
+    uint16_t clamp = ((uint16_t)(~(uint16_t)0) << gtli) & (~BITSTREAM_MASK_SIGN);
+
+    const __m512i zero = _mm512_setzero_si512();
+    const __m512i mask_sign = _mm512_set1_epi16((short)BITSTREAM_MASK_SIGN);
+    const __m512i clamp_avx = _mm512_set1_epi16(clamp);
+    const __m512i m1 = _mm512_set1_epi16(-1);
+
+    for (uint32_t coeff_idx = 0; coeff_idx < (size / 32); coeff_idx++) {
+        __m512i buff = _mm512_loadu_si512((__m512i*)(buf));
+        __m512i sign = _mm512_and_si512(buff, mask_sign);
+        buff = _mm512_and_si512(buff, clamp_avx);
+        uint32_t mask = _mm512_cmpgt_epi16_mask(buff, zero);
+        sign = _mm512_and_si512(sign, _mm512_mask_mov_epi16(zero, mask, m1));
+        buff = _mm512_or_si512(buff, sign);
+        _mm512_storeu_si512((__m512i*)(buf), buff);
+        buf += 32;
+    }
+
+    if (size % 32) {
+        uint32_t leftover = size % 32;
+        const __m128i zero_sse = _mm_setzero_si128();
+        const __m128i mask_sign_sse = _mm_set1_epi16((short)BITSTREAM_MASK_SIGN);
+        const __m128i clamp_sse = _mm_set1_epi16(clamp);
+
+        for (uint32_t coeff_idx = 0; coeff_idx < (leftover / 8); coeff_idx++) {
+            __m128i buff = _mm_loadu_si128((__m128i*)(buf));
+            __m128i sign = _mm_and_si128(buff, mask_sign_sse);
+            buff = _mm_and_si128(buff, clamp_sse);
+            sign = _mm_and_si128(sign, _mm_cmpgt_epi16(buff, zero_sse));
+            buff = _mm_or_si128(buff, sign);
+            _mm_storeu_si128((__m128i*)(buf), buff);
+            buf += 8;
+        }
+
+        for (uint32_t i = 0; i < (leftover % 8); i++) {
+            uint16_t sign = buf[i] & BITSTREAM_MASK_SIGN;
+            //remove sign and quantize
+            buf[i] &= clamp;
+            //if coeff after quantization have data, then restore sign
+            if (buf[i]) {
+                buf[i] |= sign;
+            }
+        }
+    }
+}
+
+void quantization_avx512(uint16_t* buf, uint32_t size, uint8_t* gclis, uint32_t group_size, uint8_t gtli, QUANT_TYPE quant_type) {
+    UNUSED(group_size);
+    assert(group_size == 4);
+    switch (quant_type) {
+    case QUANT_TYPE_UNIFORM:
+        quant_uniform_sse4_1(buf, size, gclis, gtli);
+        return;
+    case QUANT_TYPE_DEADZONE:
+        quant_deadzone_avx512(buf, size, gtli);
+        return;
+    default:
+        assert("unknown quantization");
+    }
+}
