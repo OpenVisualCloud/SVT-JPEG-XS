425601aa5a72dc106ebe6b61f834f69ddffe8ee4
zLukas <lukasz.grab@intel.com>
2025-05-21 08:11:33 +0000

    add ffpeg plugin build

---
+++ .github/scripts/build_ffmpeg_plugin.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+
+
+#!/bin/bash
+# filepath: /home/labrat/lgrab/SVT-JPEG-XS/ffmpeg-plugin/build_ffmpeg_svtjpegxs.sh
+
+set -e
+
+# Usage: ./build_ffmpeg_svtjpegxs.sh <jpeg-xs-repo-path> <ffmpeg-version: 6.1|7.0>
+JPEGXS_REPO=${1:-$(pwd)}
+FFMPEG_VERSION=${2:-6.1}
+INSTALL_FMPEG=${3:-"n"}
+
+
+
+if [[ "$FFMPEG_VERSION" != "6.1" && "$FFMPEG_VERSION" != "7.0" ]]; then
+    echo "Usage: $0 <jpeg-xs-repo-path> <ffmpeg-version:> <install-ffmpeg>"
+    echo "ffmpeg-version: 6.1|7.0"
+    echo "install-ffmpeg: y|n (default: n)"
+    echo "jpeg-xs-repo-path: path to the jpeg-xs repo (default: current directory)"
+    echo "Example: $0 /path/to/jpeg-xs 6.1 y"
+    exit 1
+fi
+echo "=== 0. Create installation directory and export env variable ==="
+export INSTALL_DIR="$PWD/install-dir"
+mkdir -p "$INSTALL_DIR"
+
+
+echo "=== 1. Compile and install svt-jpegxs ==="
+cd "$JPEGXS_REPO/Build/linux"
+./build.sh install --prefix "$INSTALL_DIR"
+
+echo "=== 2. Export installation location ==="
+export LD_LIBRARY_PATH="$INSTALL_DIR/lib:${LD_LIBRARY_PATH}"
+export PKG_CONFIG_PATH="$INSTALL_DIR/lib/pkgconfig:${PKG_CONFIG_PATH}"
+
+echo "=== 3. Download/Compile FFmpeg ==="
+cd "$PWD"
+git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg-$FFMPEG_VERSION
+cd ffmpeg-$FFMPEG_VERSION
+git checkout "release/$FFMPEG_VERSION"
+
+echo "=== 4. Apply jpeg-xs plugin patches ==="
+cp "$JPEGXS_REPO/ffmpeg-plugin/libsvtjpegxs"* libavcodec/
+git am --whitespace=fix "$JPEGXS_REPO/ffmpeg-plugin/$FFMPEG_VERSION/"*.patch
+
+echo "=== 5. Configure FFmpeg ==="
+./configure --enable-libsvtjpegxs --prefix="$INSTALL_DIR" --enable-shared
+
+echo "=== 6. Build and install FFmpeg ==="
+make -j"$(nproc)"
+if [[ "$INSTALL_FMPEG" == "y" ]]; then
+    make install
+else
+    echo "=== 6.2 Skip FFmpeg installation ==="
+fi
+
+echo "=== Build complete! ==="
+echo "FFmpeg binary is in $INSTALL_DIR/bin/"
