f98b242b9176eeccf83b9cf7e4518093617807bc
zLukas <lukasz.grab@intel.com>
2025-05-23 10:15:17 +0000

    remove unnecessary export from script

--- .github/scripts/build_ffmpeg_plugin_msys.sh
+++ .github/scripts/build_ffmpeg_plugin_msys.sh
@@ -9,7 +9,6 @@ if [[ "$FFMPEG_VERSION" != "6.1" && "$FFMPEG_VERSION" != "7.0" ]]; then
     echo "Usage: $0  <ffmpeg-version: 6.1|7.0>"
     exit 1
 fi
-export ASM_NASM=$YASM_PATH
 pacman -S --noconfirm make mingw-w64-x86_64-gcc mingw-w64-x86_64-cmake mingw-w64-x86_64-yasm mingw-w64-x86_64-diffutils
 INSTALL_DIR="$PWD/install-dir"
 mkdir -p "$INSTALL_DIR"
